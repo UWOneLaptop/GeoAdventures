d367cfd009c67ea6aa2f4a12892e1d019ed74e9b
Jordan McNeal <jordanmcneal22@gmail.com>
2012-01-23 15:22:15 -0800

    Updated test classes with db

--- makeQuery.py
+++ makeQuery.py
@@ -1,73 +1,114 @@
 import sqlite3
 import random
 
+#If you want to add stuff to db enter .schema for the
+#schema to get attribute names in sqlite3.
+
 class makeQuery:
 	#stuff for connecting sqlite3
-	con = sqlite3.connect('QandA')
+	con = sqlite3.connect('QandA.db')
 	c = con.cursor()
 	
 	#initialize with country name and difficulty level.
 	def __init__(self, c, dl):
 		self.country = c
 		self.diffLevel = dl
-		self.QandA = self.getQandA()
+		self.questionList = self.getQList()
+		self.currQuestion = self.getQuestion()
+		self.answerList = self.getAList()
 	
 	#Query the db
-	#returns random row from allRows and returns the tuple.
+	#returns allRows from the query.
 	#for internal use
-	def getQ(self):
+	def getQList(self):
 		t = (self.diffLevel, self.country, )
 		#gets all rows with same country and difficulty
 		self.c.execute('SELECT id, question  FROM' + \
 		' questions WHERE level=? And name=?', t)
 		allRows = self.c.fetchall()
-		index = int(random.random()*len(allRows))
-		return allRows[index]
+		return allRows
 	
 	
 	#Query the db
-	#returns tuple containing the question and answer(s)
+	#returns the list of possible answers to the current question
 	#for internal use.
-	def getA(self):
-		qInfo = self.getQ()
+	def getAList(self):
+		qInfo = self.currQuestion
 		qid = qInfo[0]
-		q = qInfo[1]
 		t = (qid, )
 		self.c.execute('SELECT valid_ans, answer FROM'+ \
 		 ' answers WHERE qid=?', t)
 		allRows = self.c.fetchall()
-		return (q, allRows)
-
-	#returns the question and all possible answers as a tuple
-	#for internal use. 
-	def getQandA(self):
-		return self.getA()
+		return allRows
 	
 	#retruns the current question as a String
 	def getQuestion(self):
-		return self.QandA[0]
+		index = random.random() * len(self.questionList)
+		return self.questionList[int(index)]
 	
 	#generates new question in current difflevel and country
 	#catagory.
 	def newQuestion(self):
-		self.QandA = self.getQandA()
-	
-	#returns just the list of answers
-	def getAnswerList(self):
-		return self.QandA[1] 
+		self.currQuestion = self.getQuestion()
+		self.answerList = self.getAList() 	
 	
 	#finds the right answer from the list of answers.
 	def getRightAnswer(self):
 		rightAns = ""
-		for x in self.QandA[1]:
+		for x in self.answerList:
 			if x[0] == 1:
 				rightAns = x[1]
 		return rightAns
 	
-	#change the difficulty level.
-	def setDiffLevel(self, dl):
-		self.diffLevel = dl
-	
-	#change the country.
-	def setCountry(self, c):
+	#change the question topic, default level is 1.
+	def changeQuestionTopic(self, c):
 		self.country = c
+		self.changeQuestionLevel(1)
+
+	#change the question difficulty level.
+	def changeQuestionLevel(self, dl):
+		self.diffLevel = dl
+		self.questionList = self.getQList()
+		self.currQuestion = self.getQuestion()
+		self.answerList = self.getAList()
+
+	#tests if a country is present in the db. returns true if
+	#there is and false if there is not.
+	def isCountry(self, c):
+		t = (self.diffLevel, c, )
+		self.c.execute('SELECT DISTINCT name  FROM' + \
+		' questions WHERE level=? And name=?', t)
+		allRows = self.c.fetchall()
+	
+		if (len(allRows) == 0):
+			return 0
+		else:
+			return 1
+	
+	def isLevel(self, dl):
+		t = (dl, self.country, )
+		self.c.execute('SELECT DISTINCT name  FROM' + \
+		' questions WHERE level=? And name=?', t)
+		allRows = self.c.fetchall()
+	
+		if (len(allRows) == 0):
+			return 0
+		else:
+			return 1
+
+	#returns current question
+	def getCurrQuestion(self):
+		return self.currQuestion[1]
+
+	#returns just the list of answers
+	def getAnswerList(self):
+		return self.answerList
+	
+	#returns the current country
+	def getCountry(self):
+		return self.country
+
+	#returns the current difficulty level
+	def getLevel(self):
+		return self.diffLevel
+
--- questionTest.py
+++ questionTest.py
@@ -1,21 +1,70 @@
 import makeQuery
 
+#*This uses the makeQuery class to ask questions.
+#
+#*Initially it starts in question mode, answers are 
+#case sensitve right now.
+#
+#*enter 'stop' to enter command mode where you can
+#change the country and level of difficulty.
+#
+#*enter 'help' in command mode to get list of all
+#commands
+
+def questionMode(x, obj):
+	rightAns = obj.getRightAnswer()
+	while (x != 'stop' and x != 'quit'):
+
+		if(x == rightAns):
+	 		print('You are 100% correct')
+		else:
+			print('Wrong!')
+	
+		obj.newQuestion();
+		print obj.getCurrQuestion()
+		x = raw_input()
+		rightAns = obj.getRightAnswer()
+	return x
+
+def commandMode(x, obj):
+	while ( x != 'resume' and x != 'quit'):
+		print '=>',
+		x = raw_input()
+		command = x.split()
+		print command
+		if (len(command) >= 2):
+			s = ' '.join(command[1:])
+			print s
+			if(command[0] == 'changec' and obj.isCountry(s)):
+				obj.changeQuestionTopic(s)
+
+			elif(command[0] == 'changel' and obj.isLevel(int(s))):
+				obj.changeQuestionLevel(int(command[1]))
+			
+			elif(not obj.isCountry(s)):
+				print 'No country "' + s + '" in the database'
+		
+			elif(not obj.isLevel(int(s))):
+				print 'No level ' + s + ' for this country in the database'
+			else:
+				print 'invalid command or arg.'
+		elif(x == 'help'):
+			print 'changec [String] ==> changes country that you want questions for'
+			print 'changel [int] ==> change the difficulty level for the questions'					
+			print 'stop ==> when in question mode this brings you to command mode'
+			print 'resume ==> resume question mode from command mode'
+			print 'quit ==> exit program'
+	return x
+#*******************************************
 obj = makeQuery.makeQuery('Mexico',1)
 
-print obj.getQuestion()
-x = raw_input();
-rightAns = obj.getRightAnswer()
+print obj.getCurrQuestion()
+x = raw_input()
 
 #loop to ask new question in catigory specified above.
-while (x != "q"):
-
-	if(x == rightAns):
- 		print('You are 100% correct')
-	else:
-		print('Wrong!')
+while (x != 'quit'):
+	x = questionMode(x, obj)
+	x = commandMode(x, obj)
 	
-	obj.newQuestion();
-	print obj.getQuestion()
-	x = raw_input()
-	rightAns = obj.getRightAnswer()
 
+	
