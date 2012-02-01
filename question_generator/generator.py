0a7d4c5d51ab535982a98cb1b81ce4e78c5cd88f
asimov4 <asimov4@gmail.com>
2012-01-31 17:42:56 -0800

    added and cleaned csv for importation into sqlite (needs more cleaning)

--- question_generator/generator.py
+++ question_generator/generator.py
@@ -1,11 +1,32 @@
-import sqlite3
+import sqlite3 as lite
+import sys,csv
 
-conn = sqlite3.connect('countries.db')
+con = None
 
-c = conn.cursor()
+try:
+    con = lite.connect('countries.db')
+    
+    cur = con.cursor()    
+    cur.execute('select * from country')
+    
+    for row in cur:
+        print row
 
-c.execute("""insert into countries values(2, "France")""")
+    cur.execute('DROP TABLE country')
+    cur.execute('CREATE TABLE IF NOT EXISTS country (territory VARCHAR,totalkm VARCHAR,totalsqmi VARCHAR,landkm2 VARCHAR,landsqmi VARCHAR,waterkm2 VARCHAR,watersqmi VARCHAR,perwater VARCHAR,Christian VARCHAR,Muslim VARCHAR,Buddhist VARCHAR,Hindu VARCHAR,Others VARCHAR,Nonreligious VARCHAR,Population VARCHAR,CountryCode VARCHAR,LongName VARCHAR,Region VARCHAR,IncomeGroup VARCHAR,CurrencyUnit VARCHAR,Constitutionalform VARCHAR,Headofstate VARCHAR,Basisofexecutivelegitimacy VARCHAR)')
 
-conn.commit()
+    reader = csv.reader(open('countryfactsv2.csv', "rb"))
+    for territory,totalkm,totalsqmi,landkm2,landsqmi,waterkm2,watersqmi,perwater,Christian,Muslim,Buddhist,Hindu,Others,Nonreligious,Population,CountryCode,LongName,Region,IncomeGroup,CurrencyUnit,Constitutionalform,Headofstate,Basisofexecutivelegitimacy,null in reader:
+        print territory
+        cur.execute('INSERT OR IGNORE INTO country (territory,totalkm,totalsqmi,landkm2,landsqmi,waterkm2,watersqmi,perwater,Christian,Muslim,Buddhist,Hindu,Others,Nonreligious,Population,CountryCode,LongName,Region,IncomeGroup,CurrencyUnit,Constitutionalform,Headofstate,Basisofexecutivelegitimacy) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', (unicode(territory),unicode(totalkm),unicode(totalsqmi),unicode(landkm2),unicode(landsqmi),unicode(waterkm2),unicode(watersqmi),unicode(perwater),unicode(Christian),unicode(Muslim),unicode(Buddhist),unicode(Hindu),unicode(Others),unicode(Nonreligious),unicode(Population),unicode(CountryCode),unicode(LongName),unicode(Region),unicode(IncomeGroup),unicode(CurrencyUnit),unicode(Constitutionalform),unicode(Headofstate),unicode(Basisofexecutivelegitimacy)))
+        con.commit()
 
-c.close()
+except lite.Error, e:
+    
+    print "Error %s:" % e.args[0]
+    sys.exit(1)
+    
+finally:
+    
+    if con:
+        con.close()
