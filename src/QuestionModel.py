07327134dcb55fd8b0ac81c29c9b19a72cf356e3
Bryan Eastes <bryan.eastes@gmail.com>
2012-04-08 12:48:38 -0700

    Added a new directory for the database

--- src/QuestionModel.py
+++ src/QuestionModel.py
@@ -1,17 +1,30 @@
 
 class QuestionQuery:
+    """
+    A question query is an iterator that returns all possible questions that could be asked given a set of tags
+    After the question query is initialized, the getQuestions(...) method should be called to populate the itereator
+    with questions
+    each individual question is called by the method getSingleQuestion(...)
+    """
+    def __init__(self,database_path):
+        """constructor, accepts a path to the database to open"""
+        self.database = database_path
+        return None;
+    
+    def getQuestions(self,tags):
+        ''' method that queries the database, populating the question list with questions from the database
+            based on the tags.
+        '''
+        import sqlite3 as sqlite
+        con = sqlite.connect(self.database);
+        cur = con.cursor();
 
-    def __init__(self):
-        """constructor"""
-        return Non;
-        
-
-    def getQuestions(self,country):
-        '''returns one question per building'''
+        tag_list = tags.split();
+        #TODO finish working here 
         return None;#skeleton
 
-    def generateQuestions(self):
-        '''this method generates new questions per country'''
+    def generateQuestions(self,tags):
+        '''method generates new questions per country'''
         return None;#skeleton
 
     def getAnswers(self, q_id):
@@ -34,20 +47,17 @@ class Question:
         self.question = question
         self.answer = answer
         self.choices = choices
-        self.buildlingTag = buildingTag
         self.q_id = q_id
-
+        
     def getQuestion(self):
         '''returns question from database with q_id'''
         return self.question
 
     def getAnswer(self):
         '''returns answer of question'''
-        #return dataBaseCall()
         return None;#skeleton
 
     def isCorrect(self, answerAttempt):
         '''returns boolean'''
-        #return answerAttempt == dataBaseCall(rightAnsId)
         return None;#skeleton
     
