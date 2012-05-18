044e0ec9c62b5515bb6ba457ede85b6374a7b39b
Bryan Eastes <bryan.eastes@gmail.com>
2012-05-18 16:16:14 -0700

    Database works

--- src/model_test.py
+++ src/model_test.py
@@ -1,8 +1,8 @@
 from model import *
 
 m = Model(dict())
-q = m.c_query.generate_countries("mexico", 2)
-q = m.q_query.generate_questions( ("",) )
-print "RESULTS"
-print str(q)
+q = m.c_query.generate_countries("Mexico", 2)
+q = m.q_query.generate_questions( ("Mexico",) )
+print ("RESULTS")
+print (str(q))
 #print str(m.q_query.generate_questions( ["mexico"] ))
